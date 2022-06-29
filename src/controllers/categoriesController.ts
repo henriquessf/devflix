bab5d25f8f65272e2cee90cf78001474eaf7e055
Pablo Machado <pablohms1@gmail.com>
2022-06-29 14:43:44 -0300

    adicionado controller de categories

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,7 +3,12 @@ import { Category } from "../models";
 
 export const categoriesController = {
   index: async (req: Request, res: Response) => {//usando função index para passar na rota a req e res de categories
-    const categories = await Category.findAll()
+    const categories = await Category.findAll(
+      {
+        attributes: ['id','name','position'],
+        order: [['position','ASC']]
+      }
+    )
     return res.json(categories)
   }
 }
